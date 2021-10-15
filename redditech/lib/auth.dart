676affaa2001f36aa191d703435788780ba3907f
Cotax <alexandre.descoins@epitech.eu>
2021-10-15 08:47:29 +0200

    [ADD] > The connexion to localhost after getting the token

--- redditech/lib/auth.dart
+++ redditech/lib/auth.dart
@@ -1,15 +1,17 @@
-// @dart=2.9
+// @dart=2.14
 
 import 'dart:async';
 import 'dart:convert';
 import 'dart:core';
 import 'dart:io';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class TokenAsk extends StatefulWidget {
-  const TokenAsk({Key key}) : super(key: key);
+
+/*class TokenAsk extends StatefulWidget {
+  const TokenAsk({Key? key}) : super(key: key);
 
     @override
     TokenAskState createState() => TokenAskState();
@@ -25,7 +27,7 @@ class TokenAskState extends State<TokenAsk> {
     Widget build(BuildContext context) {
         return WebView(initialUrl: askAccess());
     }
-}
+}*/
 
 
 String addArgsToUrl(String baseUrl, Map<String, String> args)
@@ -34,7 +36,7 @@ String addArgsToUrl(String baseUrl, Map<String, String> args)
     args.forEach((key, value) {
        baseUrl += "$key=$value&";
     });
-    baseUrl.substring(0, baseUrl.length - 1);
+    baseUrl = baseUrl.substring(0, baseUrl.length - 1   );
     return (baseUrl);
 }
 
@@ -50,7 +52,34 @@ String askAccess()
         'duration': 'permanent',
         'scope': 'history'
     };
+    print (addArgsToUrl(url, data));
     return addArgsToUrl(url, data);
 }
 
-void main() => runApp(const MaterialApp(home: TokenAsk())) ;
+_launchURL() async {
+    String url = askAccess();
+    if (await canLaunch(url)) {
+        await launch(url);
+    } else {
+        throw 'Could not launch $url';
+    }
+}
+
+class Route extends StatelessWidget {
+    const Route({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+        return(new Scaffold(
+            body: new Center(
+                child: new RaisedButton(
+                    onPressed: _launchURL,
+                    child: new Text('Log in'),
+                ),
+            ),
+        ));
+    }
+}
+
+void main() => runApp(const MaterialApp(home: Route()));
+//void main() => runApp(const MaterialApp(home: TokenAsk())) ;
